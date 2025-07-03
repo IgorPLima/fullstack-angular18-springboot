a4efc4f572f1e493631c85cdb798c07bc9fbfd50
IgorPLima <igorpereira201425@gmail.com>
2025-07-03 15:32:01 -0300

    feat:logica de atualização de curso

--- frontend/src/app/courses/service/courses.service.ts
+++ frontend/src/app/courses/service/courses.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
 
 import { Course } from '../model/course';
-import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -22,12 +22,25 @@ export class CoursesService {
     );
   }
 
-  loadById(id: string){
+  loadById(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
 
   }
 
-  save (record: Course){
+  save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
+
 }
