6b6876778bcdce87e0306f21d0891c6dda3ed11b
IgorPLima <igorpereira201425@gmail.com>
2025-07-23 15:38:38 -0300

    Reactive form with validations

--- frontend/src/app/courses/containers/course-form/course-form.component.ts
+++ frontend/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,9 +1,9 @@
-import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, Location } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatError, MatFormFieldModule, MatHint } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
@@ -19,13 +19,27 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-course-form',
   standalone: true,
-  imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatCardModule, MatToolbarModule, MatButtonModule, MatSelectModule, MatSnackBarModule, CoursesListComponent],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    CoursesListComponent,
+    MatError,
+    MatHint,
+    CommonModule
+  ],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
 export class CourseFormComponent implements OnInit {
 
   form!: FormGroup;
+  errorMessage = signal('');
 
   constructor(
     private formBuilder: NonNullableFormBuilder,
@@ -34,11 +48,11 @@ export class CourseFormComponent implements OnInit {
     private location: Location,
     private route: ActivatedRoute
 
-    ) {
+  ) {
     this.form = this.formBuilder.group({
       _id: [''],
-      name: [''],
-      category: ['']
+      name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      category: ['', [Validators.required]]
     });
   }
 
@@ -46,7 +60,7 @@ export class CourseFormComponent implements OnInit {
     const course: Course = this.route.snapshot.data['course'];
     console.log(course)
     this.form.setValue({
-      _id:course._id,
+      _id: course._id,
       name: course.name,
       category: course.category
     })
@@ -67,8 +81,28 @@ export class CourseFormComponent implements OnInit {
     this.onCancel();
   }
 
-  onError() {
+  private onError() {
     this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5;
+      return `Tamanho minimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+     if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 200;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo inválido';
   }
 
 }
